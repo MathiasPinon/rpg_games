3d9ae56705922941edce1c2783b800a43ae4f0ad
Mathias Pinon <mathias.pinon@etudiant.univ-reims.fr>
2023-09-13 09:57:02 +0200

    Ajout de la méthode is_in_alphabet avec les tests du constructeur et de la méthode)

--- TP1/Class/Parseur.py
+++ TP1/Class/Parseur.py
@@ -2,9 +2,14 @@ class Parseur :
     alph : list
 
     def __init__(self,liste:list):
-        if liste is None :
+        if type(liste) != list :
             self.alph = [ '+', '-', '*', '/', '_', '(', ')', ' ']
         else :
             self.alph = liste
 
-    
+    def is_in_alphabet(self, c:str)->bool:
+        if c in self.alph :
+            return True
+        else :
+            return False
+
