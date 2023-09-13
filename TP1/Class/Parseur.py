db2635ef8462ba30b7f29dea3b509521fd5c68ed
Mathias Pinon <mathias.pinon@etudiant.univ-reims.fr>
2023-09-13 10:31:05 +0200

    Ajout de la méthode get_token() qui fonctionne

--- TP1/Class/Parseur.py
+++ TP1/Class/Parseur.py
@@ -1,7 +1,7 @@
 class Parseur :
     alph : list
 
-    def __init__(self,liste:list):
+    def __init__(self,liste:list=[ '+', '-', '*', '/', '_', '(', ')', ' ']):
         if type(liste) != list :
             self.alph = [ '+', '-', '*', '/', '_', '(', ')', ' ']
         else :
@@ -12,4 +12,33 @@ class Parseur :
             return True
         else :
             return False
+    def get_token(self,flux:str)->tuple:
+        chaine1 = ''
+        chaine2 = ''
+        number = ['1','2','3','4','5','6','7','8','9','0']
+        i = 0
+        if flux[0] == ' ':
+            i += 1
+        if flux[i] in self.alph:
+            chaine1 = flux
+        elif flux[i].isalnum():
+            if flux[i] in number :
+                while i < len(flux) and flux[i] in number :
+                    chaine1 += flux[i]
+                    i += 1
+                for t in range(i,len(flux)):
+                    chaine2 += flux[t]
+            else :
+                while i < len(flux) and flux[i] not in number and flux[i].isalnum():
+                    chaine1 += flux[i]
+                    i += 1
+                for t in range(i,len(flux)):
+                    chaine2 += flux[t]
+        else :
+            chaine2 = flux
+
+        return (chaine1,chaine2)
+
+
+
 
--- TP1/main.py
+++ TP1/main.py
@@ -1,6 +1,19 @@
 from Class.Parseur import Parseur
 
-test = Parseur(1)
-print(test.alph)
+p = Parseur()
 
-print(test.is_in_alphabet("*"))
+flux1 = 'a + b'
+flux2 = ' a + b'
+flux3 = 'a+b'
+flux4 = 'abcd'
+flux5 = '12ab'
+flux6 = 'ab12'
+flux7 = '@ab'
+
+print(p.get_token(flux1))  # doit afficher ('a', ' + b')
+print(p.get_token(flux2))  # doit afficher ('a', ' + b')
+print(p.get_token(flux3))  # doit afficher ('a', '+b')
+print(p.get_token(flux4))  # doit afficher ('abcd', '')
+print(p.get_token(flux5))  # doit afficher ('12', 'ab')
+print(p.get_token(flux6))  # doit afficher ('ab12', '')
+print(p.get_token(flux7))  # doit afficher ('', '@ab')
