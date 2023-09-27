a7077e09b853dfd7bc0c8ca4ebc2d57124122a6f
Mathias Pinon <mathias.pinon@etudiant.univ-reims.fr>
2023-09-27 18:13:45 +0200

    Ajout de la class Négociateur

---
+++ TP1/TP2/Négociateur.py
@@ -0,0 +1,56 @@
+from TP2.Personnage import Personnage
+from random import *
+from TP2.Charmeur import Charmeur
+from TP2.Brute import Brute
+
+
+class Négociateur(Personnage):
+
+    def __init__(self, n: str, obj: list = None):
+        Personnage.__init__(self, n, obj)
+        self.intelligence *= 1.30
+        self.obstination *= 1.30
+        self.force *= 0.70
+        self.empathie *= 0.70
+
+    def choisir_action(self, autre):
+        if type(autre) == Charmeur :
+            obj = randint(0, len(autre.objets) - 1)
+            obj_choix = autre.objets[obj]
+            return self.acheter(obj_choix, autre)
+
+        if self.intelligence > autre.inteligence or (self.intelligence == autre.inteligence and self.obstination > autre.obstination ) :
+            if self.amis[autre] > 0 :
+                i = randint(0,1)
+                if i == 0 :
+                    obj = randint(0,len(self.objets)-1)
+                    obj_choix = self.objets[obj]
+                    obj_choix.valeur *= 1.15
+                    return self.vendre(obj_choix,autre)
+                elif i == 1 :
+                    obj = randint(0,len(autre.objets)-1)
+                    obj_choix = autre.objets[obj]
+                    obj_choix.valeur *= 0.85
+                    return self.acheter(obj_choix,autre)
+            else :
+                i = randint(0, 1)
+                if i == 0:
+                    obj = randint(0, len(self.objets) - 1)
+                    obj_choix = self.objets[obj]
+                    obj_choix.valeur *= 1.30
+                    return self.vendre(obj_choix, autre)
+                if i == 1:
+                    obj = randint(0, len(autre.objets) - 1)
+                    obj_choix = autre.objets[obj]
+                    obj_choix.valeur *= 0.70
+                    return self.acheter(obj_choix, autre)
+        else:
+            i = randint(0, 1)
+            if i == 0:
+                obj = randint(0, len(self.objets) - 1)
+                obj_choix = self.objets[obj]
+                return self.vendre(obj_choix, autre)
+            elif i == 1:
+                obj = randint(0, len(autre.objets) - 1)
+                obj_choix = autre.objets[obj]
+                return self.acheter(obj_choix, autre)
