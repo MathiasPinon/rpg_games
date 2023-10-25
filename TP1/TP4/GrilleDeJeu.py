297014dc8d654dd388c0d67ab92c2db6b8a87cc2
Mathias Pinon <mathias.pinon@etudiant.univ-reims.fr>
2023-10-25 16:30:04 +0200

    Modification de la classe GrilleDeJeu

--- TP1/TP4/GrilleDeJeu.py
+++ TP1/TP4/GrilleDeJeu.py
@@ -1,9 +1,36 @@
 import tkinter
 
+from TP4.Localisable import Localisable
+
+
 class GrilleDeJeu(tkinter.Canvas):
 
     def __init__(self, root, **kwargs):
         super().__init__(root, **kwargs)
+        self._objets = set()
+        self._lieux = {}
+
+    @property
+    def lieux(self):
+        return self._lieux
+
+    def getItems(self, loc: tuple) -> set:
+        if loc in self._lieux.keys():
+            return self._lieux[loc]
+        else:
+            return set()
+
+    def ajoutItem(self, o: Localisable):
+        self._objets.add(o)
+        if o.lieu not in self._lieux.keys():
+            self._lieux[o.lieu] = {o}
+        else:
+            self._lieux[o.lieu].add(o)
+
+    def enleveItem(self, o: Localisable):
+        self._objets.remove(o)
+        self._lieux[o.lieu].remove(o)
+
 
 if __name__ == '__main__':
     # Initialisation de Tk
